574d8d60ee482546cb38bbd4e612be8a3484db7d
bosciu <bosciu@icloud.com>
2021-07-15 13:43:13 +0200

    Creazione tabella packages

---
+++ database/migrations/2021_07_15_113123_create_packages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',255);
+            $table->text('description');
+            $table->unsignedSmallInteger('price');
+            $table->string('location', 150);
+            $table->string('date', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
+
+//novembre 2021- dicembre 2021
