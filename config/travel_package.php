3c022211efe766633c674b2660fe0d0248024180
bosciu <bosciu@icloud.com>
2021-07-15 14:16:40 +0200

    Popolazione databese

---
+++ config/travel_package.php
@@ -0,0 +1,47 @@
+<?php
+return [
+    [
+        "title" => "Estate in Sardegna: 7 notti a Golfo Aranci in resort sul mare + nave (con auto inclusa) da 199€!",
+        "description" => "Hey Pirati, oggi vi proponiamo una splendida offerta per questa Estate, per tuffarvi nelle cristalline acque della rinomata località di Golfo Aranci, nella nostra splendida Sardegna.
+
+        Grazie al sito Weekendesk, abbiamo selezionato un ottimo resort che offre comodi appartamenti situati a pochi passi dal mare. Inclusi nei pacchetti anche il viaggio in nave Grimaldi da Livorno a Olbia in cabina interna, con la propria auto imbarcata.
+        
+        I soggiorni dovranno essere minimo 7 notti fino ad un massimo di 21, l'offerta è prenotabile per 2, 3 o 4 persone ed è anche possibile aggiungere bambini in fase di prenotazione.
+        
+        Non fatevi scappare questa favolosa offerta che comprende anche la cancellazione gratuita, per prenotare in tutta tranquillità!",
+        "price" => 199,
+        "location" => "Golfo Aranci, Sardegna",
+        "date" => "luglio 2021 - settembre 2021"
+    ],
+    [
+        "title" => "Estate in Croazia, a Pag! 7 notti in un bellissimo hotel 4* sul mare, con piscina da soli 168€!",
+        "description" => "Hey Pirati, abbiamo selezionato per voi un bellissimo hotel 4*, situato direttamente sul mare della meravigliosa isola di Pag, in Croazia. La struttura dispone anche di una meravigliosa piscina e la cancellazione gratuita è inclusa nella tariffa.
+
+        Tante date disponibili in Estate, anche a Ferragosto, non aspettate che i prezzi salgano e la disponibilità diminuisca, prenotate subito!",
+        "price" => 168,
+        "location" => "Isola di Pago",
+        "date" => "luglio 2021 - settembre 2021"
+    ],
+    [
+        "title" => "Weekend a Berlino: volo diretto + 3 notti in centralissimo hotel 4* con area benessere da soli 111€!",
+        "description" => "Pirati, oggi vi portiamo a Berlino per un weekend che manca da troppo tempo, con un'offerta dall'ottimo rapporto qualità/prezzo!
+
+        I pacchetti che vi proponiamo sono composti da volo diretto da più città italiane e soggiorno a cavallo del weekend presso un hotel 4 stelle con area benessere situato in posizione centralissima, situato vicino alla metro e a 800 metri da Potsdamer Platz.
+        
+        Per ulteriore tranquillità, abbiamo scelto voli modificabili senza penali e un hotel la cui prenotazione è completamente cancellabile! Che ne dite, partiamo? Voli e hotel vanno prenotati separatamente.",
+        "price" => 111,
+        "location" => "Berlino",
+        "date" => "luglio 2021 - settembre 2021"
+    ],
+    [
+        "title" => "ESTATE a Creta in 1/2 PENSIONE! Volo da tante città + 7 notti in hotel 3* vicino al mare da SOLI 163€!",
+        "description" => "Cari Pirati, vi proponiamo una vacanza senza pensieri sulla splendida isola di Creta, una delle isole greche più cariche di fascino e storia, sfruttando tariffe davvero eccezionali per tutta l'estate, anche per le settimane più richieste, Ferragosto incluso!
+
+        I pacchetti proposti comprendono voli diretti a/r da tante città e il soggiorno per 7 notti in MEZZA PENSIONE in un bell'hotel 3 stelle situato vicinissimo al mare, con bar e ristorante.
+        
+        Non perdete questa occasione e assicuratevi a prezzi super oggi, la vostra vacanza al mare per quest'estate!",
+        "price" => 163,
+        "location" => "Koutouloufári",
+        "date" => "agosto 2021 - settembre 2021"
+    ],
+];
---
+++ database/seeds/PackageSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Package;
+
+class PackageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $packages = config('travel_package');
+        foreach ($packages as $item) {
+            $package = new Package();
+            $package->title = $item['title'];
+            $package->description = $item['description'];
+            $package->price = $item['price'];
+            $package->location = $item['location'];
+            $package->date = $item['date'];
+            $package->save();
+        }
+    }
+}
